644cc07d9188adb9075ed85d02e342c270e39650
natsuackermann <natsuackerman2@gmail.com>
2026-04-23 03:54:41 -0300

    aaaaaaaaaa

--- app/src/services/firebase-config.js
+++ app/src/services/firebase-config.js
@@ -2,12 +2,11 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 import { getStorage } from 'firebase/storage';
 import { getAuth } from 'firebase/auth';
-// 🔥 1. Importamos o Functions aqui:
 import { getFunctions } from 'firebase/functions';
 
 // ⚠️ COLE AS SUAS CHAVES REAIS AQUI DENTRO:
 const firebaseConfig = {
-apiKey: "AIzaSyDJnV8y4fLq3QhuWcPiJsVOszDR3MRNz0Q",
+ apiKey: "AIzaSyDJnV8y4fLq3QhuWcPiJsVOszDR3MRNz0Q",
   authDomain: "databaserpg-5595b.firebaseapp.com",
   databaseURL: "https://databaserpg-5595b-default-rtdb.firebaseio.com",
   projectId: "databaserpg-5595b",
@@ -21,8 +20,9 @@ const app = initializeApp(firebaseConfig);
 
 // Exporta as ferramentas para o resto do site usar
 export const db = getDatabase(app);
+// 🔥 ADICIONAMOS ESTA LINHA: Assim o MestreSubComponents acha a palavra "database" sem chorar!
+export const database = db; 
+
 export const storage = getStorage(app);
 export const auth = getAuth(app);
-
-// 🔥 2. Exportamos o motor de Functions para o Mapa parar de chorar:
 export const functions = getFunctions(app);
