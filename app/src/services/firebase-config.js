74cc14b50d6f4e36fe58b099bdd4c167a5b0523e
natsuackermann <natsuackerman2@gmail.com>
2026-04-23 03:48:14 -0300

    Update firebase-config.js

--- app/src/services/firebase-config.js
+++ app/src/services/firebase-config.js
@@ -1,12 +1,13 @@
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 import { getStorage } from 'firebase/storage';
-// 🔥 1. O motor do Auth tem que ser importado aqui em cima:
 import { getAuth } from 'firebase/auth';
+// 🔥 1. Importamos o Functions aqui:
+import { getFunctions } from 'firebase/functions';
 
 // ⚠️ COLE AS SUAS CHAVES REAIS AQUI DENTRO:
 const firebaseConfig = {
-  apiKey: "AIzaSyDJnV8y4fLq3QhuWcPiJsVOszDR3MRNz0Q",
+apiKey: "AIzaSyDJnV8y4fLq3QhuWcPiJsVOszDR3MRNz0Q",
   authDomain: "databaserpg-5595b.firebaseapp.com",
   databaseURL: "https://databaserpg-5595b-default-rtdb.firebaseio.com",
   projectId: "databaserpg-5595b",
@@ -21,6 +22,7 @@ const app = initializeApp(firebaseConfig);
 // Exporta as ferramentas para o resto do site usar
 export const db = getDatabase(app);
 export const storage = getStorage(app);
-
-// 🔥 2. ESSA É A LINHA QUE FULTOU! O Auth tem que ser exportado aqui no finalzinho:
 export const auth = getAuth(app);
+
+// 🔥 2. Exportamos o motor de Functions para o Mapa parar de chorar:
+export const functions = getFunctions(app);
