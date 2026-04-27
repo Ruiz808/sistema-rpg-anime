239be0496ae0ec1d0a25a12fe1056ceaa08fcd4d
JoaoRicardo70 <lemejoaoricardo@gmail.com>
2026-04-27 02:47:01 -0300

    Sistema de Seres/Espíritos parte 1

--- app/src/components/ficha/FichaPanel.jsx
+++ app/src/components/ficha/FichaPanel.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { FichaFormProvider } from './FichaFormContext';
 import { 
     FichaBioGroup, 
+    FichaSeresSelados, 
     FichaEditorAtributos, 
     FichaReatorElemental, 
     FichaDistorcaoConceitual, 
@@ -21,6 +22,7 @@ export default function FichaPanel({ className, children }) {
                 {hasChildren ? children : (
                     <>
                         <FichaBioGroup />
+                        <FichaSeresSelados />
                         <FichaEditorAtributos />
                         <FichaReatorElemental />
                         <FichaDistorcaoConceitual />
